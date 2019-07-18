61c7849bf51a190c6a4c7c42ce083975de787890
rdudisk <pi.r.carre@hotmail.fr>
2019-07-18 14:31:14 +0200

    Euh...

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 #include "test.hpp"
 
+#include "headers/RKMK.hpp"
+
 using namespace std;
 
 int main (int argc, char* argv[]) {
@@ -33,6 +35,14 @@ int main (int argc, char* argv[]) {
 	VI vi;
 	vi.m_syst = &dls;
 	vi.midpoint_fdf();
+
+	
+	/*DiscJetSpace<float,Q,TQ> djs;
+	djs.baselinstep(0.0,0.1,20);
+
+	RKMK<Q,TQ,Lie::SO3::Group<float>,Lie::SO3::Algebra<float>> rkmk;
+	rkmk.m_space = &djs;
+	rkmk.rkmk4();*/
 
 	dls.write2csv("test.csv");
 
