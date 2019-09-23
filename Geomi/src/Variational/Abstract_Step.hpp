0cd77025cbb9a099725df916f18659b55e9d355f
rdudisk <pi.r.carre@hotmail.fr>
2019-09-23 14:16:31 +0200

    Implémentation du main() pour le Rigid Body par méthode Lie Variational, pas testé

--- Geomi/src/Variational/Abstract_Step.hpp
+++ Geomi/src/Variational/Abstract_Step.hpp
@@ -7,7 +7,8 @@ namespace Abstract {
 template <typename T_M,
 		  typename T_Q,
 		  typename T_INTERNALS,
-		  typename T_PROBLEM>
+		  typename T_PROBLEM,
+		  typename T_TQ = T_Q>
 class Step
 { 
 protected:
@@ -22,11 +23,9 @@ public:
 	~Step<T_M,T_Q,T_INTERNALS,T_PROBLEM> ()
 	{ }
 
-	/*
 	T_Q
 	posFromVel (T_Q q0, T_TQ v0)
 	{ return m_internals->posFromVel(q0,v0); }
-	*/
 
 	virtual void
 	setData (T_M h_var, T_Q q0_var, T_Q q1_var) = 0;
